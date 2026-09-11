6e6adeb4f948f8864df10ab7ba9f31f284ddea5e
d5ng <dong5ffice@gmail.com>
2026-09-11 16:50:02 +0900

    chore: 커밋 품질 검증 자동화

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
-  printWidth: 80,
-  semi: true,
-  singleQuote: false,
-  trailingComma: "all",
-};
+  printWidth: 140,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
 
-export default config;
+export default config
